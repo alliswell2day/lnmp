9290f526ba8d4b86675b4ae4395822b50b29d219
Fancy <157902039@qq.com>
2016-12-07 18:02:51 +0800

    Update ionCube.sh

--- include/ionCube.sh
+++ include/ionCube.sh
@@ -26,23 +26,23 @@ Install_ionCube()
     rm -rf /usr/local/ioncube
     cd ${cur_dir}/src
     rm -rf ioncube
-    rm -rf ioncube_loaders_lin_x8*.tar.gz
+    #rm -rf ioncube_loaders_lin_x8*.tar.gz
     if grep -Eqi "xcache.so" /usr/local/php/etc/php.ini; then
         if [ "${Is_64bit}" = "y" ] ; then
-            Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_x86-64.tar.gz ioncube_loaders_lin_x86-64.tar.gz
-            tar zxf ioncube_loaders_lin_x86-64.tar.gz
+            #Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_x86-64.tar.gz ioncube_loaders_lin_x86-64.tar.gz
+            tar zxf ioncube_loaders_lins_x86-64.tar.gz
         else
             cd ${cur_dir}/src
-            Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_x86.tar.gz ioncube_loaders_lin_x86.tar.gz
-            tar zxf ioncube_loaders_lin_x86.tar.gz
+            #Download_Files ${Download_Mirror}/web/ioncube/4.7.5/ioncube_loaders_lin_x86.tar.gz ioncube_loaders_lin_x86.tar.gz
+            tar zxf ioncube_loaders_lins_x86.tar.gz
         fi
     else
         if [ "${Is_64bit}" = "y" ] ; then
-            Download_Files http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz ioncube_loaders_lin_x86-64.tar.gz
+            #Download_Files http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz ioncube_loaders_lin_x86-64.tar.gz
             tar zxf ioncube_loaders_lin_x86-64.tar.gz
         else
             cd ${cur_dir}/src
-            Download_Files http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz ioncube_loaders_lin_x86.tar.gz
+            #Download_Files http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86.tar.gz ioncube_loaders_lin_x86.tar.gz
             tar zxf ioncube_loaders_lin_x86.tar.gz
         fi
     fi
